170649d8f1d10f0e74cb77b6d172abdb55114348
Md. Azizul Hakim <aazizul924@gmail.com>
2026-09-23 23:55:57 +0600

    added search controller

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,4 +1,4 @@
-const { getTrendingMovies, getPopularMovies } = require("../services/tmdbService")
+const { getTrendingMovies, getPopularMovies, searchMovies: searchMoviesFromTMDB } = require("../services/tmdbService")
 
 const getTrending = async (req, res) => {
     try {
@@ -38,4 +38,36 @@ const getPopular = async (req, res) => {
     }
 }
 
-module.exports = { getTrending, getPopular }
+const searchMovies = async (req, res) => {
+    try {
+        const { query } = req.query
+
+        if (!query) {
+            return res.status(400).json({
+                success: false,
+                message: "Search query is required"
+            })
+        }
+
+        const movies = await searchMoviesFromTMDB(query)
+
+        res.json({
+            success: true,
+            movies
+        })
+    }
+    catch (error) {
+        console.error(error.message)
+
+        res.status(500).json({
+            success: false,
+            message: "Failed to search movies"
+        })
+    }
+}
+
+module.exports = {
+    getTrending,
+    getPopular,
+    searchMovies
+}
