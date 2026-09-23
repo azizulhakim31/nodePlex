aab4ec5e808402d433023a2d361e2c5578b44554
Md. Azizul Hakim <aazizul924@gmail.com>
2026-09-23 23:57:57 +0600

    search router added

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const { getTrending, getPopular } = require("../controllers/movieController")
+const { getTrending, getPopular, searchMovies } = require("../controllers/movieController")
 
 const router = express.Router()
 
 router.get("/trending", getTrending)
 router.get("/popular", getPopular)
+router.get("/search", searchMovies)
 
 module.exports = router
