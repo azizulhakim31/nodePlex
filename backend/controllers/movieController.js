8002f40a925e51e883c232940d95e3f4cfbbb69b
Md. Azizul Hakim <aazizul924@gmail.com>
2026-09-26 16:25:32 +0600

    added movie details controller

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,4 +1,4 @@
-const { getTrendingMovies, getPopularMovies, searchMovies: searchMoviesFromTMDB } = require("../services/tmdbService")
+const { getTrendingMovies, getPopularMovies, searchMovies: searchMoviesFromTMDB, getMovieDetails } = require("../services/tmdbService")
 
 const getTrending = async (req, res) => {
     try {
@@ -66,8 +66,30 @@ const searchMovies = async (req, res) => {
     }
 }
 
+const getMovie = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const movie = await getMovieDetails(id)
+
+        res.json({
+            success: true,
+            movie
+        })
+    }
+    catch (error) {
+        console.error(error.message)
+
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch movie details"
+        })
+    }
+}
+
 module.exports = {
     getTrending,
     getPopular,
-    searchMovies
+    searchMovies,
+    getMovie
 }
--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { getTrending, getPopular, searchMovies } = require("../controllers/movieController")
+const { getTrending, getPopular, searchMovies, getMovie } = require("../controllers/movieController")
 
 const router = express.Router()
 
 router.get("/trending", getTrending)
 router.get("/popular", getPopular)
 router.get("/search", searchMovies)
+router.get("/:id", getMovie)
 
 module.exports = router
